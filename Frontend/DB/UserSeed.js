0e7347df186b0a1eaa625863c601069ae7bcafe0
Alon Segev <94148094+alonx5050@users.noreply.github.com>
2023-08-22 18:11:47 +0300

    Add files via upload

---
+++ Frontend/DB/UserSeed.js
@@ -0,0 +1,33 @@
+[{
+    "_id": {
+        "$oid": "647e274b961b8485a84dbdc5"
+    },
+    "fullName": "Alon Segev",
+    "email": "alonx5050@example.com",
+    "password": "A1234567890",
+    "address": "Tel aviv",
+    "isAdmin": true
+},
+{
+    "_id": {
+        "$oid": "647e274b961b8485a84dbdc7"
+    },
+    "fullName": "Shlomi karavani",
+    "email": "shlomi@example.com",
+    "password": "Ashlomi1996",
+    "address": "Rishon lezion",
+    "orders": [{ "_id": "647e2a7e961b8485a84oocb1" }],
+    "isAdmin": false
+
+},
+{
+    "_id": {
+        "$oid": "647e274b961b8485a84dbdc8"
+    },
+    "fullName": "Roee Shemesh",
+    "email": "roee@example.com",
+    "password": "Aroee1996",
+    "address": "Raanana",
+    "orders": [{ "_id": "647e2a7e961b8485a84oocb1" }],
+    "isAdmin": false
+}]
