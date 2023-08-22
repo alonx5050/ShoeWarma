afb9a47c7e68c234d9e403065af01deee8cc8b5e
Alon Segev <94148094+alonx5050@users.noreply.github.com>
2023-08-22 17:34:33 +0300

    Add files via upload

---
+++ Backend/models/ProductSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+    frontImage: { type: String, required: true },
+    category: { type: String, required: true },
+    brand: { type: String, required: true },
+    details: [{
+        size: { type: String, required: true },
+        quantityInStock: { type: Number, required: true }
+    }],
+    images:
+    {
+        image1: { type: String },
+        image2: { type: String },
+        image3: { type: String },
+        image4: { type: String },
+    },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
---
+++ Backend/models/UserSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    address: { type: String, default: 'no address' },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    orders: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Order'
+        }
+    ]
+
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
